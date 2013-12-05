49625199ca1027eadffe5e787530709cab728615
RaymondMcT <raymond@quandl.com>
2013-12-05 15:44:58 -0500

    Multiset functionality added

--- +Quandl/get.m
+++ +Quandl/get.m
@@ -43,7 +43,20 @@ function [output headers] = get(code, varargin)
     type = p.Results.type;
     authcode = p.Results.authcode;
     
-    string = strcat('http://www.quandl.com/api/v1/datasets/',code,'.csv?sort_order=asc');
+    if strcmp(class(code), 'char')
+        string = strcat('http://www.quandl.com/api/v1/datasets/',code,'.csv?sort_order=asc');
+    elseif strcmp(class(code), 'cell') 
+        if prod(size(code)) == 1
+            string = strcat('http://www.quandl.com/api/v1/datasets/',code,'.csv?sort_order=asc');
+        else
+            code = regexprep(code, '/', '.')
+            for i = 2:length(code)
+                code{i} = strcat(',', code{i});
+            end
+            string = strcat('http://www.quandl.com/api/v1/multisets.csv?columns=',[code{:}], '&sort_order=asc');
+        end
+    end
+        
     % Check for authetication token in inputs or in memory.
     if size(authcode) == 0
         'It would appear you arent using an authentication token. Please visit http://www.quandl.com/help/matlab or your usage may be limited.'
